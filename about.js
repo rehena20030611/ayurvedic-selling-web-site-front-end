35343dbea0a273df66a830843f651f8e801c9e1a
Rehena Supasan <supasan20030611@gmail.com>
2026-09-17 16:12:33 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+
+      <h1>About Us</h1>
+
+      <p>Welcome to My Website!</p>
+
+      <p> We are a simple website created using React.
+        Our goal is to provide useful information and
+        a simple and friendly user experience.</p>
+
+    </div>
+  );
+}
+
+export default About;
---
+++ services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Services() {
+  return (
+
+    <div>
+      <h1>Service page</h1>
+      <p>Welcome to my website.</p>
+    </div>
+  );
+}
+
+export default Services;
